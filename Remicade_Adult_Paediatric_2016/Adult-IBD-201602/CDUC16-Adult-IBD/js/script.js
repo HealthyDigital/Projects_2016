fd7cbfbbade262819f1ed6d2a369d6e6cf55c69c
Creative3 <bene@healthydigital.net>
2016-03-17 17:51:25 +1300

    Update: - - completed P-IBD1610-Acute-severe, 	tiding up and nav scripting: 8.2 hrs => total: 80.4 hrs

--- Remicade_Adult_Paediatric_2016/Adult-IBD-201602/CDUC16-Adult-IBD/js/script.js
+++ Remicade_Adult_Paediatric_2016/Adult-IBD-201602/CDUC16-Adult-IBD/js/script.js
@@ -8,7 +8,8 @@ $( function(){
 		menu = $('.main-menu'),
 		ref = $('.ref'),
 		study = $('.study-design'),
-		footerMenu = $('.footer-menu > li');
+		footerMenu = $('.footer-menu > li'),
+		currentSection = localStorage.getItem('currentSection');
 		
 	//show/hide refs & study
 	$(".popup-menu > li:not(':first-child, .disabled, .btn-mpi')").on('tap', function(){
@@ -42,9 +43,30 @@ $( function(){
 			span.prev().fadeIn();
 		});
     });
+	//change to Paed menu
 	
-	//go to PI 
-	goToSlide('btn-mpi', 'CrohnsDisease000.zip');
+	if(!$.isEmptyObject(currentSection)){
+		currentSection = currentSection.toLowerCase();
+		wrapper.addClass(currentSection);
+		if(currentSection === 'paediatric-ibd'){
+			menu.find('.cd ul > li:last-child').html('Ulcerative<br />Colitis');
+		}else{
+			menu.find('.cd ul > li:last-child').html('Fistulising<br />Crohn’s disease');
+		}
+	}
+	
+	//go to slide 
+	goToSlide('btn-mpi', 'AP-IBD1618-PI');
+	goToSlide('main-menu [data-slide=cd]', 'CD1601-Disease-course');
+	goToSlide('main-menu [data-slide=fist]', 'CD1614-Fistula-healing');
+	goToSlide('main-menu [data-slide=ms], .main-menu [data-slide=uc]', 'UC1601-Goals-of-treatment');
+	goToSlide('main-menu [data-slide=as]', 'UC1612-Three-dose-induction');
+	goToSlide('main-menu [data-slide=st]', 'AP-IBD1601-Safety');
+	goToSlide('main-menu [data-slide=do]', 'AP-IBD1609-Medsafe-approved');
+	goToSlide('main-menu [data-slide=ad]', 'AP-IBD1611-Facts');
+	goToSlide('main-menu [data-slide=pr]', 'AP-IBD1614-Patient-choice');
+	goToSlide('main-menu [data-slide=home] > span', 'Remicade-for-IBD');
+	
 	
 	//remove animated
 	function removeAnimation(e, f){
@@ -58,24 +80,42 @@ $( function(){
 	
 });
 
-//navigation
-function hideShowElements(btn, e){
-	"use strict";
-	btn.not('.disabled').on('tap', function(){
-		var ele = e;
-		$('.reference:visible, .study-design:visible').hide();
-		if(ele.is(':hidden')){
-			ele.fadeIn();
-		}else{
-			ele.fadeOut();
-		}
-	});
-}
 //go to slide
-function goToSlide(btn, url){
+function goToSlide(btn, asset){
 	"use strict";
 	$('.'+btn).on('tap', function(){
-		var id = $(this).parents('.ref').length > 0 ? ', DCREF' : '';
-		document.location = 'veeva:gotoSlide('+url+'.zip'+ id +')';
+		var $this = $(this),
+			id = '',
+			data = $this.attr('data-slide'); 
+		if($this.parents('.ref').length > 0){
+			id = ', AP_IBD_REFS';
+		}else if ($('.paediatric-ibd').length && data === 'cd'){
+			asset = 'P-IBD1601-Response-and-remission'; 
+			id = ', P_IBD2016';
+			
+		}else if ($('.paediatric-ibd').length && data === 'fist'){
+			asset = 'P-IBD1606-Clinical-remission'; 
+			id = ', P_IBD2016';
+
+		}else if($this.parent().attr('data-slide') === 'home'){
+			id = $this.index();
+			switch($this.index()){
+				case 1:
+					asset = 'CDUC16-Adult-IBD'; 
+					id = ', A_IBD2016';
+				break;
+				case 2:
+					asset = 'Paediatric-IBD'; 
+					id = ', P_IBD2016';
+				break;
+				default:
+					asset = 'Remicade-for-IBD'; 
+					id = ', AP_IBD2016';
+				break;
+			}
+			localStorage.setItem('currentSection', asset);
+		}
+		
+		document.location = 'veeva:gotoSlide('+asset+'.zip'+ id +')';
 	});
 }
--- Remicade_Adult_Paediatric_2016/Paediatric-IBD-201602/Paediatric-IBD/js/script.js
+++ Remicade_Adult_Paediatric_2016/Paediatric-IBD-201602/Paediatric-IBD/js/script.js
@@ -43,8 +43,15 @@ $( function(){
 		});
     });
 	
-	//go to PI 
-	goToSlide('btn-mpi', 'CrohnsDisease000.zip');
+	//go to slide 
+	goToSlide('btn-mpi', 'AP-IBD1618-PI');
+	goToSlide('main-menu [data-slide=cd]', 'P-IBD1601-Response-and-remission');
+	goToSlide('main-menu [data-slide=uc]', 'P-IBD1606-Clinical-remission');
+	goToSlide('main-menu [data-slide=st]', 'AP-IBD1601-Safety');
+	goToSlide('main-menu [data-slide=do]', 'AP-IBD1609-Medsafe-approved');
+	goToSlide('main-menu [data-slide=ad]', 'AP-IBD1611-Facts');
+	goToSlide('main-menu [data-slide=pr]', 'AP-IBD1614-Patient-choice');
+	goToSlide('main-menu [data-slide=home] > span', 'Remicade-for-IBD');
 	
 	//remove animated
 	function removeAnimation(e, f){
@@ -58,24 +65,30 @@ $( function(){
 	
 });
 
-//navigation
-function hideShowElements(btn, e){
-	"use strict";
-	btn.not('.disabled').on('tap', function(){
-		var ele = e;
-		$('.reference:visible, .study-design:visible').hide();
-		if(ele.is(':hidden')){
-			ele.fadeIn();
-		}else{
-			ele.fadeOut();
-		}
-	});
-}
 //go to slide
-function goToSlide(btn, url){
+function goToSlide(btn, asset){
 	"use strict";
 	$('.'+btn).on('tap', function(){
-		var id = $(this).parents('.ref').length > 0 ? ', DCREF' : '';
-		document.location = 'veeva:gotoSlide('+url+'.zip'+ id +')';
+		var $this = $(this),
+			id = ''; 
+		if($this.parents('.ref').length > 0){
+			id = ', AP_IBD_REFS';
+		}else if($this.parent().attr('data-slide') === 'home'){
+			id = $this.index();
+			switch($this.index()){
+				case 1:
+					asset = 'CDUC16-Adult-IBD'; id = ', A_IBD2016';
+				break;
+				case 2:
+					asset = 'Paediatric-IBD'; id = ', P_IBD2016';
+				break;
+				default:
+					asset = 'Remicade-for-IBD'; id = ', AP_IBD2016';
+				break;
+			}
+			localStorage.setItem('currentSection', asset);
+		}
+		//console.log(localStorage.getItem('currentSection'));
+		document.location = 'veeva:gotoSlide('+asset+'.zip'+ id +')';
 	});
 }
