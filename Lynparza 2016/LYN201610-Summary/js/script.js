a2a0553f8539ffd73239d53d7d794c978d699a8f
Creative3 <bene@healthydigital.net>
2016-06-09 17:50:22 +1200

    no message

--- Lynparza 2016/LYN201610-Summary/js/script.js
+++ Lynparza 2016/LYN201610-Summary/js/script.js
@@ -4,46 +4,85 @@ $( function(){
 	$(document).on('touchmove',function(e){
  		e.preventDefault();
 	});
-	/*var container = $('#container'),
+	var content = $('.content'),
+		navTop = $('.nav-top'),
 		overlay = $('.overlay'),
-		ref = overlay.find('.ref'),
+		video =overlay.find('.video'),
+		items = $('.overlay > div'),
+		btn = $('.content > .btn, .nav-top [data-href=menu]'),
+		btnMenu = $('.nav-top [data-href=menu]'),
 		info = overlay.find('.info'),
-		nav = overlay.find('.nav'),
-		currentSection = localStorage.getItem('currentSection');*/
-	
-	//close menu
-/*	$('.close').on('tap', function(){
-		var t = $(this).parent();
-			t.addClass('bounceOutUp');
-		//removeAnimation(t, true);
+		btnClose = overlay.find('.arrow, .close');
+		
+		btn.on('tap', function(){
+			var $this = $(this);
+				applyShade(overlay);
+				overlay.addClass('show');
+				
+				items.hide();
+			if($this.hasClass('btn-info')){
+				overlay.find(".info > *:not('.arrow')").hide();
+				info.show().find('[data-info='+$this.parent().find('.swiper-slide-active').attr('data-slide')+']').show();
+				//console.log($this.parent().find('.swiper-slide-active').attr('data-slide'));
+			}else if($this.hasClass('btn-play')){
+				video.show();
+				overlay.find('video').get(0).play();
+			}else {
+				
+				overlay.find('.nav').show();
+				btnMenu.css('opacity', 0);
+				
+			}
+		});
+		//close overlay
+		btnClose.on('tap', function(){
+			overlay.find('video').get(0).pause();
+			btnMenu.css('opacity', 1);
+			overlay.removeClass('show');
+			applyShade(overlay);
+		});	
+	//top menu go to slide >>
+	$('.nav-top').on('tap', "li:not('[data-href=menu]')", function(){
+		var $this = $(this),
+			km = '', id = '';
+			console.log($this.attr('data-href'))
+			switch($this.attr('data-href')){
+				case "warning":
+					km = 'LYN201612-Contraindications';
+				break;
+				case "pi":
+					km = 'LYN2016-PI';
+					id = 'LYNREF_2016';
+				break;
+				case "bibliography":
+					km = 'LYN201611-References';
+				break;
+				case "resource":
+					km = 'LYN201613-Resources';
+				break;
+				default:
+					km = 'LYN2016-Test-or-Treat-me';
+				break;
+			}
+		goToSlide(km, id);
 	});
-	*/
-	//go to slide 
-	//goToSlide('btn-mpi', 'AP-IBD1618-PI');
-	
-	
-	//remove animated
-	/*function removeAnimation(e, f){
-		//console.log(e);
-		setTimeout( function(){
-			wrapper.find('.animated').removeClass('bounceInDown bounceOutUp zoomInDown zoomIn');
-			if(!$.isEmptyObject(e) && f){ e.hide(); }
-		}, 800);
-	}
-	removeAnimation();*/
-	
+		
 });
 
 //go to slide
-function goToSlide(btn, asset){
+function goToSlide(km, id){
 	"use strict";
-	$('.'+btn).on('tap', function(){
-		var $this = $(this),
-			id = '',
-			data = $this.attr('data-slide'); 
-		
-		//document.location = 'veeva:gotoSlide('+asset+'.zip'+ id +')';
-	});
+	//$('.'+btn).on('tap', function(){});
+	id = id ? ', '+id : '';
+	document.location = 'veeva:gotoSlide('+km+'.zip'+id+')';
+	
 }
 //global timeline
 var tl = new TimelineLite();
+
+///add shade class
+function applyShade(e){
+	setTimeout( function(){
+		e.toggleClass('shade');
+	}, 400);
+}
