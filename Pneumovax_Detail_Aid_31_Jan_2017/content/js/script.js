b458fc7b8209446a3ceebceac09e391f9e3cefb6
Creative3 <bene@healthydigital.net>
2017-03-01 11:05:41 +1300

    packaged for Seqirus

---
+++ Pneumovax_Detail_Aid_31_Jan_2017/content/js/script.js
@@ -0,0 +1,94 @@
+// JavaScript Document
+$( function(){
+	//global variables 
+	var slide = $('.slide'),
+		footer = $('footer'),
+		info = $('.nav [data-href=pi], .nav [data-href=ref]'),
+        overlay = $('.overlay');
+	
+	$('.arrow').on('tap', function(){
+		var t = $(this);
+		info.removeClass('active');
+		footer.removeClass('info').find('.item').hide();
+		if(!t.hasClass('prev')){
+			t.parent().toggleClass('up');
+		}
+	})
+	
+	$('.nav li').on('tap', function(){
+		var $this = $(this), 
+			file = $this.attr('data-href');
+			
+			if(file === 'pi' || file === 'ref'){
+				info.removeClass('active');
+				$this.addClass('active');
+				footer.addClass('info').find('.item').hide();
+				
+				if(file === 'pi'){
+					footer.find('.item.pi').slideDown();
+					
+				}else{
+					footer.find('.item.refs').slideDown();
+				}
+			}else{
+              
+				file = !slide.hasClass('home') && file === 'index' ? '../'+file : 
+                  !slide.hasClass('home') && file !== 'index' ? '../'+file+'/'+file : 
+                   slide.hasClass('home') && file === 'index' ? file : file+'/'+file;
+				goToSlide(file);
+			}
+    })
+	$('.review > span, .view-pdf').on('tap', function(){
+		//console.log(slide.hasClass('home'))
+		if(slide.hasClass('home')){
+			openPDF('pdf/pi');
+		} else{
+			openPDF('../pdf/pi');
+		}
+	})
+    $('.btn-chart').on('tap', function(){
+        overlay.fadeIn().addClass('show')
+        .find('.close').on('tap', function(){
+            overlay.removeClass('show').fadeOut();
+        });
+    })
+	
+/*	$('span.pi_link').on('tap', function(){
+		if(slide.hasClass('home')){
+		//console.log(slide.hasClass('home'))
+			openPDF('pdf/Dukoral_PI_AU2');
+		} else {
+			openPDF('../pdf/Dukoral_PI_AU2');
+		}
+	})*/
+	
+	/*$('.handbook').on('tap', function() {
+		if(slide.hasClass('home')) {
+			//console.log(slide.hasClass('home'))
+			openPDF('pdf/2.CholeraImmunisation Handbook10thEdition');
+		} else {
+			openPDF('../pdf/2.CholeraImmunisation Handbook10thEdition');
+		}
+	})*/
+	//navigate();
+});
+
+function goToSlide(url){
+	window.location= url+'.html';
+}
+function openPDF(url){
+	window.location= url+'.pdf';
+}
+function navigate(prev, next){
+	var url = '';
+	$('.slide').on('swipeleft swiperight', function(e){
+		url = e.type === 'swipeleft' ? next : prev;
+		//
+		if(url){
+          url = url === 'index' ? '../'+url : $('.slide').hasClass('home') ? url+'/'+url : '../'+url+'/'+url;
+          goToSlide(url);
+        }
+		
+	})
+	
+}
